f50fde180d376c542e1f310f95f28b84644a5e2d
SuperRavenSn1per <70824779+SuperRavenSn1per@users.noreply.github.com>
2021-09-18 01:40:46 -0400

    Update rpu.lua

--- ravenpack/rpu.lua
+++ ravenpack/rpu.lua
@@ -15,3 +15,20 @@ function centerWrite(text, color, highlight, yOffset)
   term.setTextColor(currentTxt)
   term.setBackgroundColor(currentBg)
 end
+
+function centerWriteTop(text, color, highlight)
+  local w,h = term.getSize()
+  
+  local currentTxt = term.getTextColor()
+  local currentBg = term.getBackgroundColor()
+  
+  if color == nil then color = currentTxt end
+  if highlight == nil then highlight = currentBg end
+  
+  term.setCursorPos(math.floor(w / 2 - string.len(text) / 2), 1)
+  term.setTextColor(color)
+  term.setBackgroundColor(highlight)
+  write(text)
+  term.setTextColor(currentTxt)
+  term.setBackgroundColor(currentBg)
+end
